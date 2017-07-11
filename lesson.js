7eb49d71f7388e7d26e5d7e3bdcf9cabf70573b7
lVovaNl <vladimir.shlyakhovskiy@gmail.com>
2017-07-11 17:10:20 +0300

    js

--- lesson.js
+++ lesson.js
@@ -1,1 +1,8 @@
-//coment
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:1337/');
